ca083c8501d5efb73e6f1e6e19e66ea17c2548ed
MyatLinn <you@example.com>
2019-08-27 15:48:41 +0630

    dsa

--- src/app/components/add-product/add-product.component.ts
+++ src/app/components/add-product/add-product.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {Product} from '../../models/product-model';
-import {ProductService} from '../../services/product.service';
-import {AngularFireStorage} from '@angular/fire/storage';
-import {AppConstants} from '../../utils/app-constants';
+import { Product } from '../../models/product-model';
+import { ProductService } from '../../services/product.service';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { AppConstants } from '../../utils/app-constants';
 
 @Component({
   selector: 'app-add-product',
@@ -12,39 +12,38 @@ import {AppConstants} from '../../utils/app-constants';
 export class AddProductComponent implements OnInit {
 
 
-      private newProduct: Product = new Product();
-      private productAdded: boolean;
-      private categories :{ name: string; id: number }[] =AppConstants.categories;
-      private targetSize :{ size: string; id: number }[] =AppConstants.targetSize;
-      constructor(
-        private productService:ProductService,
-        private storage:AngularFireStorage
+  private newProduct: Product = new Product();
+  private productAdded: boolean;
+  private categories: { name: string; id: number }[] = AppConstants.categories;
+  private targetSize: { size: string; id: number }[] = AppConstants.targetSize;
+  constructor(
+    private productService: ProductService,
+    private storage: AngularFireStorage
 
-      ) { }
+  ) { }
 
-      ngOnInit() {
-      }
+  ngOnInit() {
+  }
 
-      onSubmit(){
+  onSubmit() {
+    this.productService.newProduct(this.newProduct).subscribe(
+      res => {
+        console.log(res)
+        this.productAdded = true;
+        this.newProduct = new Product();
+      },
+      error => console.log(error)
+    );
+  }
 
-         this.productService.newProduct(this.newProduct).subscribe(
-           res => {
-
-             console.log(res)
-             this.productAdded = true;
-             this.newProduct=new Product();
-           },
-            error => console.log(error)
-         );
-      }
-
-      handleFileInput(file:File){
-        let uniqKey = 'pic' + Math.floor(Math.random()* 10000);
-        const fileUp= this.storage.upload('/photos/'+uniqKey,file);
-        fileUp.then( a => a.ref.getDownloadURL().then(a => {this.newProduct.imageUrl=a
-          this.newProduct.isAvailable=true
-          console.log(a);
-        }));
-      }
+  handleFileInput(file: File) {
+    let uniqKey = 'pic' + Math.floor(Math.random() * 10000);
+    const fileUp = this.storage.upload('/photos/' + uniqKey, file);
+    fileUp.then(a => a.ref.getDownloadURL().then(a => {
+      this.newProduct.imageUrl = a
+      this.newProduct.isAvailable = true
+      console.log(a);
+    }));
+  }
 
 }
--- src/app/components/edit-product/edit-product.component.ts
+++ src/app/components/edit-product/edit-product.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {Product} from '../../models/product-model';
-import {ActivatedRoute, Params, Router} from '@angular/router';
-import {ProductService} from '../../services/product.service';
-import {AppConstants} from '../../utils/app-constants';
-import {AngularFireStorage} from '@angular/fire/storage';
+import { Product } from '../../models/product-model';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { ProductService } from '../../services/product.service';
+import { AppConstants } from '../../utils/app-constants';
+import { AngularFireStorage } from '@angular/fire/storage';
 
 @Component({
   selector: 'app-edit-product',
@@ -12,47 +12,47 @@ import {AngularFireStorage} from '@angular/fire/storage';
 })
 export class EditProductComponent implements OnInit {
 
-      private productId : number;
-      private product:Product =new Product();
-      private productUpdate:boolean;
-      private categories :{ name: string; id: number }[] =AppConstants.categories;
-      private targetSize :{ size: string; id: number }[] =AppConstants.targetSize;
+  private productId: number;
+  private product: Product = new Product();
+  private productUpdate: boolean;
+  private categories: { name: string; id: number }[] = AppConstants.categories;
+  private targetSize: { size: string; id: number }[] = AppConstants.targetSize;
 
-      constructor(private storage:AngularFireStorage,
-                  private router:Router,
-                  private route:ActivatedRoute,
-                  private getProductService:ProductService) { }
+  constructor(private storage: AngularFireStorage,
+    private route: ActivatedRoute,
+    private getProductService: ProductService) { }
 
-      ngOnInit() {
-        this.route.params.forEach( (params:Params) =>{
-          this.productId = Number.parseInt(params['id'])
-        });
+  ngOnInit() {
+    this.route.params.forEach((params: Params) => {
+      this.productId = Number.parseInt(params['id'])
+    });
 
-        this.getProductService.getProduct(this.productId).subscribe(
-          (data:any) =>{
-            this.product = data
-            console.log(this.product)
-          },
-          error => {
-            console.log(error)
-          }
-        )
+    this.getProductService.getProduct(this.productId).subscribe(
+      (data: any) => {
+        this.product = data
+        console.log(this.product)
+      },
+      error => {
+        console.log(error)
       }
+    )
+  }
 
-      onSubmit() {
-        this.getProductService.editPrdouct(this.product).subscribe(
-          data => {
-            console.log(data)
-            this.productUpdate=true;
-          },
-          error => console.log(error)
-        );
-      }
+  onSubmit() {
+    this.getProductService.editPrdouct(this.product).subscribe(
+      data => {
+        console.log(data)
+        this.productUpdate = true;
+      },
+      error => console.log(error)
+    );
+  }
 
-  handleFileInput(file:File){
-    let uniqKey = 'pic' + Math.floor(Math.random()* 10000);
-    const fileUp= this.storage.upload('/photos/'+uniqKey,file);
-    fileUp.then( a => a.ref.getDownloadURL().then(a => {this.product.imageUrl=a
+  handleFileInput(file: File) {
+    let uniqKey = 'pic' + Math.floor(Math.random() * 10000);
+    const fileUp = this.storage.upload('/photos/' + uniqKey, file);
+    fileUp.then(a => a.ref.getDownloadURL().then(a => {
+      this.product.imageUrl = a
       console.log(a);
     }));
   }
