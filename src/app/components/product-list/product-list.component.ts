42251486a116b025d67a8779991c4514534a375a
MyatLinn <you@example.com>
2019-08-28 21:56:19 +0630

    da

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, DoCheck, OnChanges, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product } from '../../models/product-model';
 import { ProductService } from '../../services/product.service';
 import { MatDialog, MatDialogRef } from '@angular/material';
@@ -16,6 +16,7 @@ export class ProductListComponent implements OnInit {
   private productList: Product[];
   private allChecked: boolean;
   private removeBookList: Product[] = new Array();
+  private check: boolean;
   constructor(
     private router: Router,
     private productService: ProductService,
@@ -31,9 +32,9 @@ export class ProductListComponent implements OnInit {
       (data: any) => {
         console.log(data),
           this.productList = data;
+          this.check = this.productList.length > 0 ? false : true;
       },
-
-      error => console.log(error)
+      console.log
     );
   }
 
@@ -61,25 +62,6 @@ export class ProductListComponent implements OnInit {
     this.router.navigate(['/viewProduct', this.selectedProduct.id]);
   }
 
-  openDialog(book: any) {
-    let dialog = this.dialog.open(DialogResultExampleDialog);
-    dialog.afterClosed().subscribe(
-      result => {
-        if (result == 'yes') {
-          this.productService.deleteProduct(book.id).subscribe(
-            res => {
-              console.log(res);
-              this.ngOnInit();
-            },
-            err => {
-              console.log(err)
-            }
-          )
-        }
-      }
-    )
-  }
-
   removeSelectedBooks() {
     if (this.removeBookList.length > 0) {
       let dialog = this.dialog.open(DialogResultExampleDialog);
@@ -90,10 +72,11 @@ export class ProductListComponent implements OnInit {
               this.productService.deleteProduct(book.id).subscribe(
                 res => {
                   console.log(res);
-                  this.getProducts();
                 },
                 err => {
-                  console.log(err)
+                  console.log(err);
+                  this.getProducts();
+                  this.removeBookList = [];
                 });
             }
           }
@@ -101,13 +84,8 @@ export class ProductListComponent implements OnInit {
     } else {
       let dialog = this.dialog.open(DialogErrorExampleDialog);
     }
- 
-
   }
 
-  // ngDoCheck(){
-  //    this.getProducts()
-  // }
 
 
 
